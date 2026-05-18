ee83b6235d70f0973d297f8b4ab060888bb90c9e
icondenimStore <tn696199@gmail.com>
2026-05-19 00:31:07 +0700

    fix: UI modal customer, user

--- springserver/src/main/java/com/hotel/controllers/admin/PaymentController.java
+++ springserver/src/main/java/com/hotel/controllers/admin/PaymentController.java
@@ -1,10 +1,14 @@
 package com.hotel.controllers.admin;
 
+import com.hotel.entity.Customer;
+import com.hotel.entity.Payment;
 import com.hotel.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,7 +19,16 @@ public class PaymentController {
 
     @GetMapping("/payments")
     public String paymentView(Model model) {
+        model.addAttribute("payment", new Payment());
         model.addAttribute("payments", paymentService.getAllPayments());
+        return "payment";
+    }
+
+    @PostMapping("/payments")
+    public String processPayment(@ModelAttribute(name = "payment") Payment payment) {
+        System.out.println(payment.getId());
+        System.out.println(payment.getBooking().getId());
+        System.out.println(payment.getStaff().getId());
         return "payment";
     }
 }
--- springserver/src/main/java/com/hotel/controllers/admin/UserController.java
+++ springserver/src/main/java/com/hotel/controllers/admin/UserController.java
@@ -1,10 +1,15 @@
 package com.hotel.controllers.admin;
 
+import com.hotel.entity.Customer;
+import com.hotel.entity.User;
+import com.hotel.enums.RoleUser;
 import com.hotel.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,8 +25,17 @@ public class UserController {
 
     @GetMapping("/users")
     public String userView(Model model) {
-        model.addAttribute("users", userService.getAllUsers());
+        model.addAttribute("user", new User());
+        model.addAttribute("roleUsers", RoleUser.values());
+        model.addAttribute("listUsers", userService.getAllUsers());
+        return "user";
+    }
 
+    @PostMapping("/users")
+    public String processUser(@ModelAttribute(name = "user") User user) {
+        System.out.println(user.getId());
+        System.out.println(user.getUsername());
+        System.out.println(user.getActive());
         return "user";
     }
 }
