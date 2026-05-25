5bc81f50d5fac4eec75259a89609f478b9a53d26
Nguyễn Tuấn <tuan-nv0505@MacBook-Pro-cua-Nguyen.local>
2026-05-25 14:11:44 +0700

    fix: .gitignore

--- springserver/src/main/java/com/hotel/utils/JwtUtils.java
+++ springserver/src/main/java/com/hotel/utils/JwtUtils.java
@@ -9,11 +9,7 @@ import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.MACVerifier;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
 
 import java.util.Date;
 
